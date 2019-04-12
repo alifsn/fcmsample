02123148212853077c91ff1165890eb3d8137386
Reza Agustiana <reza.agustiana@gmail.com>
2019-04-12 09:32:21 +0700

    fix

--- app/src/main/java/com/reza/fcmsample/MainActivity.kt
+++ app/src/main/java/com/reza/fcmsample/MainActivity.kt
@@ -72,45 +72,6 @@ Log.d("REZAAA", "onCreate")
             })
     }
 
-    @Throws(IOException::class, JSONException::class)
-    fun addToGroup(
-        senderId: String,
-        userEmail: String,
-        registrationId: String,
-        idToken: String
-    ): String {
-        val url = URL("https://fcm.googleapis.com/fcm/googlenotification")
-        val con = url.openConnection() as HttpURLConnection
-        con.doOutput = true
-
-        // HTTP request header
-        con.setRequestProperty("project_id", senderId)
-        con.setRequestProperty("Content-Type", "application/json")
-        con.setRequestProperty("Accept", "application/json")
-        con.requestMethod = "POST"
-        con.connect()
-
-        // HTTP request
-        val data = JSONObject()
-        data.put("operation", "add")
-        data.put("notification_key_name", userEmail)
-        data.put("registration_ids", JSONArray(arrayListOf(registrationId)))
-        data.put("id_token", idToken)
-
-        val os = con.outputStream
-        os.write(data.toString().toByteArray(charset("UTF-8")))
-        os.close()
-
-        // Read the response into a string
-        val `is` = con.inputStream
-        val responseString = Scanner(`is`, "UTF-8").useDelimiter("\\A").next()
-        `is`.close()
-
-        // Parse the JSON string and return the notification key
-        val response = JSONObject(responseString)
-        return response.getString("notification_key")
-    }
-
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         setIntent(intent)
